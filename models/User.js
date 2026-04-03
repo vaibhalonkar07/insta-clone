eb700e3f8baff7421c51a57c019c229430e25d83
vaibhalonkar07 <132092289+vaibhalonkar07@users.noreply.github.com>
2026-04-03 18:36:15 +0530

    Add user schema for mongoose model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,13 @@
+const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema({
+  identifier: String,
+  identifierType: String,
+  password: String, // plain text now
+  fullName: { type: String, default: "" },
+  profilePicture: { type: String, default: "" },
+  isActive: { type: Boolean, default: true },
+  lastLogin: Date,
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
